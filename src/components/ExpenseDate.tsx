8fe2acd501ede58214258b36a18a9ada4980279c
Jbsselim <jbsselim@gmail.com>
2023-04-17 11:29:34 -0400

    styling the ExpenseDate component

--- src/components/ExpenseDate.tsx
+++ src/components/ExpenseDate.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 
+import "./ExpenseDate.css";
+
 const ExpenseDate: React.FC<{ date: Date }> = ({ date }) => {
   const month = date.toLocaleString("fr-FR", { month: "long" });
   const day = date.toLocaleString("fr-FR", { day: "2-digit" });
   const year = date.getFullYear();
 
   return (
-    <div>
-      <div>{day}</div>
-      <div>{month}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
     </div>
   );
 };
