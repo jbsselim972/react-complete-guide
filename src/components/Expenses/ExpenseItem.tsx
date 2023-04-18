f37a4642a8a2fe803a465ae456da521d40ac6c05
Jbsselim <jbsselim@gmail.com>
2023-04-18 05:22:11 -0400

    states

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem: React.FC<Expense> = ({ title, amount, date }) => {
+  const [newTitle, setTitle] = useState(title);
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+
   return (
     <>
       <Card className="expense-item">
         <ExpenseDate date={date} />
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2>{newTitle}</h2>
         </div>
         <div className="expense-item__price">{amount} €</div>
+        <button onClick={clickHandler}>Change Title</button>
       </Card>
     </>
   );
