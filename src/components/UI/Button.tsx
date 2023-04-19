ddadc007f0ad3f6457197dfbfdf481d91ed2adfb
Jbsselim <jbsselim@gmail.com>
2023-04-19 06:38:08 -0400

    using css modules

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,44 +1,45 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
+import styles from "./Button.module.css";
 
-// interface ButtonProps {
-//   type: HTMLButtonElement["type"] | undefined;
-//   onClick?: () => void;
-//   children: React.ReactNode;
-// }
+interface ButtonProps {
+  type: HTMLButtonElement["type"] | undefined;
+  onClick?: () => void;
+  children: React.ReactNode;
+}
 
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
-`;
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+// `;
 
-// const Button: React.FC<ButtonProps> = ({ type, onClick, children }) => {
-//   return (
-//     <button type={type} className="button" onClick={onClick}>
-//       {children}
-//     </button>
-//   );
-// };
+const Button: React.FC<ButtonProps> = ({ type, onClick, children }) => {
+  return (
+    <button type={type} className={styles.button} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
