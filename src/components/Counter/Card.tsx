d0445bb6a8f45201fc47e9978d98cdf0e4f467d9
Jbsselim <jbsselim@gmail.com>
2023-04-26 10:37:34 -0400

    deleting

--- src/components/Counter/Card.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-import classes from "./Card.module.css";
-
-const Card: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className={classes.card}>{children}</div>;
-};
-
-export default Card;
--- src/components/UI/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import classes from "./Card.module.css";
-
-const Card: React.FC<CardProps> = ({ children, className }) => {
-  return <div className={`${classes.card} ${className}`}>{children}</div>;
-};
-
-export default Card;
