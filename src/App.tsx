2770fd341202bd69f45cc744bf3648af5975cc31
Jbsselim <jbsselim@gmail.com>
2023-04-27 11:31:44 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ import ExpenseTracker from "./ExpenseTracker/ExpenseTracker";
 import CourseGoals from "./CourseGoals/CourseGoals";
 import LoginPage from "./LoginPage/LoginPage";
 import TaskManager from "./TasksManager/TaskManager";
+import CounterRedux from "./CounterRedux/CounterRedux";
 
 const App: React.FC = () => {
-  return <TaskManager />;
+  return <CounterRedux />;
 };
 
 export default App;
---
+++ src/CounterRedux/CounterRedux.tsx
@@ -0,0 +1,9 @@
+import Counter from "./components/Counter";
+
+import "./CounterRedux.css";
+
+function CounterRedux() {
+  return <Counter />;
+}
+
+export default CounterRedux;
