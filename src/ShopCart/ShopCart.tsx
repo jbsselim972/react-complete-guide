ef78c8b62697bf3231d9b626bbd0afbcd7a2e57e
Jbsselim <jbsselim@gmail.com>
2023-05-02 11:59:03 -0400

    using an action creator thunk

--- src/ShopCart/ShopCart.tsx
+++ src/ShopCart/ShopCart.tsx
@@ -6,60 +6,26 @@ import Products from "./components/Product/Products";
 import "./ShopCart.css";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./store";
-import useHttp from "../shared/hooks/use-http";
-import { uiActions } from "./store/ui-slice";
 import Notification from "../shared/UI/Notification";
-
+import { sendCartData } from "./store/cart-slice";
+let isInitial = true;
 const ShopCart: FC = () => {
   const showCart = useSelector((state: RootState) => state.ui.cartIsVisible);
   const cart = useSelector((state: RootState) => state.cart);
   const notification = useSelector((state: RootState) => state.ui.notification);
   const dispatch = useDispatch();
-  const { isLoading, error, sendRequest } = useHttp();
-  const [isInitial, setIsInitial] = useState(true);
-
-  useEffect(() => {
-    if (isInitial) {
-      setIsInitial(false);
-      return;
-    }
-    sendRequest({
-      url: import.meta.env.VITE_BACKEND_URL + "/cart.json",
-      method: "PUT",
-      body: cart,
-    });
-  }, [cart, sendRequest]);
 
   useEffect(() => {
     console.log("render");
-    if (isLoading && !error) {
-      dispatch(
-        uiActions.showNotification({
-          title: "Sending...",
-          status: "pending",
-          message: "Sending cart data...",
-        })
-      );
-    } else if (!error && !isLoading) {
-      dispatch(
-        uiActions.showNotification({
-          title: "Success",
-          status: "success",
-          message: "Sent cart data successfully",
-        })
-      );
+
+    if (isInitial) {
+      console.log("first");
+      isInitial = false;
+      return;
     }
 
-    if (error) {
-      dispatch(
-        uiActions.showNotification({
-          title: "Error",
-          status: "error",
-          message: "Sending cart data failed",
-        })
-      );
-    }
-  }, [isLoading, dispatch, error]);
+    dispatch<any>(sendCartData(cart));
+  }, [cart, dispatch]);
 
   return (
     <Layout>
--- src/ShopCart/store/cart-slice.ts
+++ src/ShopCart/store/cart-slice.ts
@@ -1,6 +1,8 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { AnyAction, PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { Item } from "../components/Cart/CartItem";
 import { Product } from "../components/Product/Products";
+import { uiActions } from "./ui-slice";
+import { Dispatch } from "react";
 
 export type CartState = {
   items: Item[];
@@ -53,6 +55,51 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart: CartState) => {
+  return async (dispatch: Dispatch<AnyAction>) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        import.meta.env.VITE_BACKEND_URL + "/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          title: "Success",
+          status: "success",
+          message: "Sent cart data successfully",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          title: "Error",
+          status: "error",
+          message: "Sending cart data failed",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
