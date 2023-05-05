ba1e754cfde4faa3ed2850a81ad99b9ea740a1d0
Jbsselim <jbsselim@gmail.com>
2023-05-05 10:23:31 -0400

    linking between pages

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+
 const News = () => {
-  return <div>News</div>;
+  return (
+    <>
+      <h1>The news page</h1>
+      <ul>
+        <li>
+          <Link href={"/news/1"}>News 1</Link>
+        </li>
+        <li>
+          <Link href={"/news/2"}>News 2</Link>
+        </li>
+        <li>
+          <Link href={"/news/3"}>News 3</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default News;
