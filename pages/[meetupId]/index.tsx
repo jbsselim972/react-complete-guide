aaa58099dcf9f292cc31341a166498dda69198b1
Jbsselim <jbsselim@gmail.com>
2023-05-06 17:13:34 -0400

    getting meetup details data & preparing pages

--- pages/[meetupId]/index.tsx
+++ pages/[meetupId]/index.tsx
@@ -1,28 +1,49 @@
-import MeetupDetail from "@/components/meetups/MeetupDetail";
 import { GetStaticPaths, GetStaticProps } from "next";
+import { MongoClient, ObjectId } from "mongodb";
+import MeetupDetail from "@/components/meetups/MeetupDetail";
 
 const MeetupDetails = ({ meetupData }: { meetupData: Meetup }) => {
   return <MeetupDetail {...meetupData} />;
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const mongoUrl = process.env.MONGO_URI!;
+  const client = await MongoClient.connect(mongoUrl);
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+  const meetupsIds = await meetupsCollection
+    .find({}, { projection: { _id: 1 } })
+    .toArray();
+  console.log(meetupsIds);
+  client.close();
   return {
     fallback: false,
-    paths: [{ params: { meetupId: "m1" } }, { params: { meetupId: "m2" } }],
+    paths: meetupsIds.map((meetupId) => ({
+      params: { meetupId: meetupId._id.toString() },
+    })),
   };
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const meetupId = context.params?.meetupId;
+  const meetupId = context.params!.meetupId as string;
+  const mongoUrl = process.env.MONGO_URI!;
+  const client = await MongoClient.connect(mongoUrl);
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+  const meetupData = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+  client.close();
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        title: "A First Meetup",
-        image:
-          "https://mediaim.expedia.com/destination/1/ac13a74176bac2e6435c3c8288c192d3.jpg",
-        address: "Some Street 5, 12345 Some City",
-        description: "This is a first meetup",
+        id: meetupData?._id.toString(),
+        title: meetupData?.title,
+        image: meetupData?.image,
+        address: meetupData?.address,
+        description: meetupData?.description,
       },
     },
   };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,30 +3,6 @@ import { MongoClient } from "mongodb";
 import { GetStaticProps, NextPage } from "next";
 import MeetupList from "@/components/meetups/MeetupList";
 
-const MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://mediaim.expedia.com/destination/1/ac13a74176bac2e6435c3c8288c192d3.jpg",
-    address: "Some Address",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://mediaim.expedia.com/destination/1/ac13a74176bac2e6435c3c8288c192d3.jpg",
-    address: "Some Address2",
-  },
-  {
-    id: "m3",
-    title: "A Third Meetup",
-    image:
-      "https://mediaim.expedia.com/destination/1/ac13a74176bac2e6435c3c8288c192d3.jpg",
-    address: "Some Address3",
-  },
-];
-
 const HomePage: NextPage<{ meetups: Meetup[] }> = ({ meetups }) => {
   return <MeetupList meetups={meetups} />;
 };
