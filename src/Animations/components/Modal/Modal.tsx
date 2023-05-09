f8485d1c0938c55538829a6d89e728b053449160
Jbsselim <jbsselim@gmail.com>
2023-05-09 08:44:35 -0400

    animation timings

--- src/Animations/components/Modal/Modal.tsx
+++ src/Animations/components/Modal/Modal.tsx
@@ -3,9 +3,14 @@ import { FC } from "react";
 import classes from "./Modal.module.css";
 import { Transition } from "react-transition-group";
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
 const Modal: FC<{ show: boolean; closed: () => void }> = ({ closed, show }) => {
   return (
-    <Transition mountOnEnter unmountOnExit in={show} timeout={300}>
+    <Transition mountOnEnter unmountOnExit in={show} timeout={animationTiming}>
       {(state) => (
         <div
           className={`${classes.modal} 
