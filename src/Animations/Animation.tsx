9490d3d7a619481252f033f5a4b5b833e52d3ca0
Jbsselim <jbsselim@gmail.com>
2023-05-09 08:25:20 -0400

    using ReactTansitionGroup

--- src/Animations/Animation.tsx
+++ src/Animations/Animation.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from "react";
+import Transition from "react-transition-group/Transition";
 import List from "./components/List/List";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -7,7 +8,7 @@ import "./Animation.css";
 
 const Animation: FC = () => {
   const [openModal, setOpenModal] = useState(false);
-
+  const [showBlock, setShowBlock] = useState(false);
   const showModal = () => {
     setOpenModal(true);
   };
@@ -19,6 +20,27 @@ const Animation: FC = () => {
   return (
     <div className="App">
       <h1>React Animations</h1>
+      <button
+        className="Button"
+        onClick={() => setShowBlock((prevState) => !prevState)}
+      >
+        Toggle
+      </button>
+      <br />
+      <Transition in={showBlock} timeout={1000} mountOnEnter unmountOnExit>
+        {(state) => (
+          <div
+            style={{
+              backgroundColor: "red",
+              width: "100px",
+              height: "100px",
+              margin: "auto",
+              transition: "opacity 1s ease-out",
+              opacity: state === "exited" ? 0 : 1,
+            }}
+          />
+        )}
+      </Transition>
       <Modal closed={hideModal} show={openModal} />
       <Backdrop show={openModal} />
       <button className="Button" onClick={showModal}>
